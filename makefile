a4250ec9a9c41dcb032dda9937dbe0869139e906
Oriononmar <luo78910@gmail.com>
2026-03-05 09:40:40 -0500

    MAKE!

---
+++ makefile
@@ -0,0 +1,21 @@
+BUILD := build
+BIN := $(BUILD)/bin
+
+AS := nasm
+LD := ld
+ASFLAGS := -f elf64
+
+V001_SRC := v0.01/v001.asm
+V001_O := $(BIN)/v001.o
+V001 := $(BIN)/v001
+
+include v0.01/makefile
+
+$(BUILD):
+	mkdir -p $@
+
+$(BIN): | $(BUILD)
+	mkdir -p $@
+
+all: $(V001) | $(BIN)
+	cd $(BIN) && ./v001
--- v0.01/v001.asm
+++ v0.01/v001.asm
@@ -1,19 +1,67 @@
 DEFAULT REL
 
 SECTION .data
-msg DB "Hello, World!", 10
-len EQU $ - msg
+filename DB "test.asm",0
+
+SECTION .bss
+buffer RESB 1024
 
 SECTION .text
 GLOBAL _start
 
 _start:
-    MOV RAX, 1 ;; WRITE
-    MOV RDI, 1 ;; STDOUT
-    MOV RSI, msg ;; ADDRESS OF THE MESSAGE
-    MOV RDX, len ;; LENGTH OF THE MESSAGE
-    SYSCALL ;; MAKE THE SYSTEM CALL
+    ;; ------------------------
+    ;; OPEN(FILENAME, O_RDONLY)
+    ;; ------------------------
+    MOV RAX, 2                ;; SYS_OPEN
+    LEA RDI, [rel filename]   ;; FILE TO OPEN (RIP-relative)
+    XOR RSI, RSI              ;; FLAGS = O_RDONLY (0)
+    XOR RDX, RDX              ;; MODE = (not used)
+    SYSCALL
+    MOV R12, RAX              ;; FILE DESCRIPTOR
 
-    MOV RAX, 60 ;; EXIT
-    XOR RDI, RDI ;; EXIT CODE 0
-    SYSCALL ;; MAKE THE SYSTEM CALL
+    CMP R12, 0                ;; CHECK IF FILE OPENED
+    JS exit_error             ;; NEGATIVE = ERROR
+
+read_loop:
+    ;; ------------------------
+    ;; READ(FD, BUFFER, 1024)
+    ;; ------------------------
+    MOV RAX, 0               ;; SYS_READ
+    MOV RDI, R12             ;; FILE DESCRIPTOR
+    LEA RSI, [rel buffer]    ;; BUFFER (RIP-relative)
+    MOV RDX, 1024            ;; BYTES TO READ
+    SYSCALL
+
+    CMP RAX, 0               ;; EOF?
+    JE done                  ;; IF ZERO, WE'RE DONE
+
+    ;; ------------------------
+    ;; WRITE(1, BUFFER, RAX)
+    ;; ------------------------
+    MOV RDI, 1               ;; STDOUT
+    MOV RDX, RAX             ;; BYTES TO WRITE (RETURNED BY READ)
+    MOV RAX, 1               ;; SYS_WRITE
+    SYSCALL
+
+    JMP read_loop
+
+done:
+    ;; ------------------------
+    ;; CLOSE(FD)
+    ;; ------------------------
+    MOV RAX, 3               ;; SYS_CLOSE
+    MOV RDI, R12             ;; FD
+    SYSCALL
+
+    ;; ------------------------
+    ;; EXIT(CODE)
+    ;; ------------------------
+    MOV RAX, 60              ;; SYS_EXIT
+    XOR RDI, RDI             ;; EXIT CODE 0
+    SYSCALL
+
+exit_error:
+    MOV RAX, 60              ;; SYS_EXIT
+    MOV RDI, 1               ;; EXIT CODE 1 (ERROR)
+    SYSCALL
